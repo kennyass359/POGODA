02a3a42db18495bd63bd16c9c75894462060db57
kennyass359 <78080655+kennyass359@users.noreply.github.com>
2024-01-31 13:06:55 +0300

    jan31

--- app.js
+++ app.js
@@ -1,57 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = "216e02695fd914c41334334f40b06080";
-    let apiUrl =
-        "https://api.openweathermap.org/data/2.5/weather?q=yourcity&appid=" +
-        apiKey;
+  const apiKey = "216e02695fd914c41334334f40b06080";
+  let apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=yourcity&appid=" +
+      apiKey;
 
-    function updateForecast(city = "yourcity") {
-        apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  function updateForecast(city = "yourcity") {
+      apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=ru`;
 
-        fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.main && data.main.temp) {
-                    const temperatureElement = document.getElementById("temperature");
-                    const windSpeedElement = document.getElementById("wind-speed");
-                    const humidityElement = document.getElementById("humidity");
-            
-                    const temperature = Math.round(data.main.temp - 273.15);
-                    const windSpeed = data.wind.speed;
-                    const humidity = data.main.humidity;
-            
-                    temperatureElement.textContent = "Температура: " + temperature + "°C";
-                    windSpeedElement.textContent = "Скорость ветра: " + windSpeed + " км/ч";
-                    humidityElement.textContent = "Влажность: " + humidity + "%";
-            
-                    // Проверим наличие coord в данных
-                    if (data.coord && data.coord.lat && data.coord.lon) {
-                        // Обновим карту с новыми координатами
-                        updateMap(data.coord.lat, data.coord.lon);
-                    } else {
-                        console.error("Ошибка: Невозможно получить координаты из данных");
-                    }
-                } else {
-                    console.error("Ошибка: Невозможно получить данные о температуре");
-                }
-            })
-            .catch((error) => console.error("Ошибка при получении данных:", error));
-            
-    } // Закрываем функцию updateForecast
+      fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+              if (data.main && data.main.temp) {
+                  const temperatureElement = document.getElementById("temperature");
+                  const windSpeedElement = document.getElementById("wind-speed");
+                  const humidityElement = document.getElementById("humidity");
+                  const weatherDescriptionElement = document.getElementById("weather-description");
+          
+                  const temperature = Math.round(data.main.temp - 273.15);
+                  const windSpeed = data.wind.speed;
+                  const humidity = data.main.humidity;
+                  const weatherDescription = data.weather[0].description;
+          
+                  temperatureElement.textContent = "Температура: " + temperature + "°C";
+                  windSpeedElement.textContent = "Скорость ветра: " + windSpeed + " км/ч";
+                  humidityElement.textContent = "Влажность: " + humidity + "%";
+                  weatherDescriptionElement.textContent = "Погодные условия: " + weatherDescription;
+          
+                  if (data.coord && data.coord.lat && data.coord.lon) {
+                      updateMap(data.coord.lat, data.coord.lon);
+                  } else {
+                      console.error("Ошибка: Невозможно получить координаты из данных");
+                  }
+              } else {
+                  console.error("Ошибка: Невозможно получить данные о погоде");
+              }
+          })
+          .catch((error) => console.error("Ошибка при получении данных:", error));
+          
+  }
 
-    const refreshButton = document.getElementById("refresh-button");
-    refreshButton.addEventListener("click", function () {
-        const cityInput = document.getElementById("city-input");
-        const city = cityInput.value.trim();
-        if (city !== "") {
-            updateForecast(city);
-        } else {
-            alert("Пожалуйста, введите город.");
-        }
-    });
+  const refreshButton = document.getElementById("refresh-button");
+  refreshButton.addEventListener("click", function () {
+      const cityInput = document.getElementById("city-input");
+      const city = cityInput.value.trim();
+      if (city !== "") {
+          updateForecast(city);
+      } else {
+          alert("Пожалуйста, введите город.");
+      }
+  });
 
-    updateForecast();
-    initMap(56.109693, 47.262652); // Пример координат для initMap
+  updateForecast(); // Вызываем функцию для получения погоды при загрузке страницы
+  initMap(56.109693, 47.262652); // Пример координат для initMap
 });
+
+
   
 let map;
 
@@ -153,4 +156,6 @@ function updateMap(latitude, longitude) {
     document.getElementById("popup").style.display = "none";
     document.body.style.overflow = "auto"; // Enable scrolling
   }
+
+  
   
